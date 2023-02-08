d2023b932306b2006641218559b117ea00ff719f
kristina <kris-malkina@mail.ru>
2023-02-08 21:45:10 +0300

    #3 мы создали пустой сайт для курса валют

--- bublik/kursvalut/views.py
+++ bublik/kursvalut/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-
-
+def currency_conversion(request):
+    currency_original = request.GET.get("currency_original")
+    currency_required = request.GET.get("currency_required")
+    quantity = request.GET.get("quantity")
+    return HttpResponse(f"исходная: {currency_original} требуемая: {currency_required} количество {quantity} </h2>")
 
 
 # Create your views here.
